428a2a80d8e32cb73eb2ac45d09eb604c3329e66
rla-xogud <tae.h.kim@outlook.com>
2021-10-04 02:56:54 -0500

    Completed RPS with image changes

--- RPS.js
+++ RPS.js
@@ -1,76 +1,98 @@
 let options = [
     "rock",
-    "raper",
+    "paper",
     "scissors"
 ]
 
-const btn = document.querySelector(".gameButton");
-btn.addEventListener('click', ()=>{alert("hello world")});
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+rockBtn.addEventListener('click', ()=>{play('rock', computerPlay())});
+paperBtn.addEventListener('click', ()=>{play('paper', computerPlay())});
+scissorsBtn.addEventListener('click', ()=>{play('scissors', computerPlay())});
+resetBtn.addEventListener('click', ()=>{resetGame()});
+
+
 function computerPlay(){
     return options[Math.floor(Math.random() *3)];
 }
 
 function play(player, computer){
-    if(player == null){
-        alert("Please enter your choice");
+    if(isGameValid()){
+        alert("Current game is over, please press reset if you want to play");
         return;
     }
-    let cast = player.toLowerCase();
-    if(!(cast == "rock"||cast == "scissors"||cast == "paper")){
-        alert("Invalid choice");
-        return;
-    }
-    switch (cast){
+    updateImages(player, computer)
+    switch (player){
         case "rock":
             if(computer == "paper"){
-                return -1;
+                gameResult("comp")
             }else if(computer == "scissors"){
-                return 1;
+                gameResult("player")
             }
+            break;
         case "scissors":
             if(computer == "paper"){
-                return 1;
+                gameResult("player")
             }else if(computer == "rock"){
-                return -1;
+                gameResult("comp")
             }
+            break;
         case "paper":
             if(computer == "rock"){
-                return 1;
+                gameResult("player")
             }else if(computer == "scissors"){
-                return -1;
+                gameResult("comp")
             }
+            break;
         default:
-            return 0;
             break;
     }
 }
+function updateImages(playerImage, computerImage){
+    let player = document.querySelector("#playerCast");
+    player.src = playerImage+".jpeg";
 
-function game(){
-    let playerWins = 0;
-    let computerWins = 0;
-    while(playerWins < 5 || computerWins < 5){
-        let player = prompt("What is your choice? Pick rock, paper, or scissors");
-        if(player === null){
-            alert("Thanks for playing, if you want to play again refresh the page");
-            return;
-        }
-        let computer = computerPlay();
-        let result = play(player,computer);
-        if(result == -1){
-            computerWins++;
-            alert("The computer won this round. Computer wins: " + computerWins + " Player wins: " + playerWins);
-        }else if(result == 1){
-            playerWins++;
-            alert("You won this round Computer wins: "+ computerWins + " Player wins: " + playerWins);
-        }else{
-            alert("No winner! Computer wins: "+ computerWins + " Player wins: " + playerWins);
+    let computer = document.querySelector("#computerCast");
+    computer.src = computerImage +".jpeg";
+
+}
+function gameResult(winner){
+    if(winner == "comp"){
+        let computerWins = document.querySelector("#compScore");
+        let score = Number(computerWins.textContent);
+        score++;
+        computerWins.textContent = score;
+        if(score == 5){
+            alert("Computer wins!")
         }
     }
-    if(playerWins == 5){
-        console.log("You won the game!");
-    }else{
-        console.log("The computer won the game!");
+    if(winner == "player"){
+        let playerWins = document.querySelector("#playerScore");
+        let score = Number(playerWins.textContent);
+        score++;
+        playerWins.textContent = score;
+        if(score == 5){
+            alert("Player wins!")
+        }
     }
 }
-game();
-    
+
+function resetGame(){
+    let playerWins = document.querySelector("#playerScore");
+    let computerWins = document.querySelector("#compScore");
+
+    playerWins.textContent = "0";
+    computerWins.textContent = "0";
+
+    updateImages("none", "none")
+    alert("New game! Have Fun!");
+}
+
+function isGameValid(){
+    let playerWins = document.querySelector("#playerScore").textContent;
+    let computerWins = document.querySelector("#compScore").textContent;
+    return(playerWins >= 5 || computerWins >= 5)        
+}
